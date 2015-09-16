a27705e66f15168265a3cf116fe1aa2ba3f8209d
beldmi <belyakovdima92@gmail.com>
2015-09-16 21:51:31 +0300

    Field to be size flexible

--- src/xo/hexlet/model/Field.java
+++ src/xo/hexlet/model/Field.java
@@ -1,20 +1,24 @@
 package xo.hexlet.model;
 
-import xo.hexlet.model.exceptions.AlreadyOccupitedException;
 import xo.hexlet.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
 public class Field {
 
-    private static final int FIELD_SIZE = 3;
-    private static final int MAX_COORDINAT = FIELD_SIZE;
     private static final int MIN_COORDINAT = 0;
 
-    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+    private final Figure[][] field;
+
+    private final int filedSize;
+
+    public Field(int fieldSize) {
+        this.filedSize = fieldSize;
+        field = new Figure[filedSize][filedSize];
+    }
 
     public int getSize() {
-        return FIELD_SIZE;
+        return filedSize;
     }
 
     public Figure getFigure(Point point) throws InvalidPointException {
@@ -32,11 +36,11 @@ public class Field {
     }
 
     private boolean checkPoint(final Point point) {
-        return checkCoordinate(point.x) && checkCoordinate(point.y);
+        return checkCoordinate(point.x, field.length) && checkCoordinate(point.y, field[point.x].length);
     }
 
-    private boolean checkCoordinate(final int coordinate) {
-        return coordinate >= MIN_COORDINAT && coordinate < MAX_COORDINAT;
+    private boolean checkCoordinate(final int coordinate, final int maxCoordinate) {
+        return coordinate >= MIN_COORDINAT && coordinate < maxCoordinate;
     }
 
 }
